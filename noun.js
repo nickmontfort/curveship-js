c84000352b08c91da3b3486fc1cfaa0280126ba3
Angela Chang <anjchang@gmail.com>
2021-03-24 21:34:32 -0400

    First pass at irregular plural nouns

---
+++ noun.js
@@ -0,0 +1,70 @@
+//  Copyright 2021 anjchang
+//
+// licensed under the GNU General Public License v3.0. See the file LICENSE
+// for complete terms.
+//
+// Irregular plural noun dictionary
+// Nouns that change vowels (foot-> feet)
+// Nouns that alter the base stem (mouse->mice)
+// Nouns that don't change when pluralized (sheep,fish)
+// Nouns that are plurals of Latin and Greek (ends in -is/es/on/um/ix)
+
+// Not covered gendered nouns blond/blonde, fiancé/fiancée
+
+const plurals = {
+	"aircraft": ["aircreft"],
+	"alumnus": ["alumni"],
+	"analysis": ["analyses"],
+	"appendix": ["appendices"],
+	"axis": ["axes"],
+	"bacterium": ["bacteria"],
+	"basis": ["bases"],
+	"buffalo": ["buffalo"],
+	"cactus": ["cacti"],
+	"child": ["children"],
+	"crisis": ["crises"],
+	"criterion": ["criteria"],
+	"curriculum": ["curricula"],
+	"data": ["datum"],
+	"deer": ["deer"],
+	"die": ["dice"],
+	"diagnosis": ["diagnoses"],
+	"fish": ["fish"],
+	"focus": ["foci"],
+	"foot": ["feet"],
+	"fungus": ["fungi"],
+	"goose": ["geese"],
+	"half": ["halves"],
+	"hovercraft": ["hovercreft"],
+	"hippopotamus": ["hippopotami"],
+	"index": ["indices"],
+	"loaf": ["loaves"],
+	"man": ["men"],
+	"memorandum": ["memoranda"],
+	"moose": ["moose"],
+	"mouse": ["mice"],
+	"nucleus": ["nuclei"],
+	"octopus": ["octopi"],
+    "ox":  ["oxen"],
+    "phenomenon": ["phenomena"],
+    "person": ["people"],
+    "radius": ["radii"],
+	"scarf": ["scarves"],
+	"self": ["selves"],
+	"series": ["series"],
+	"sheep": ["sheep"],
+	"shrimp": ["shrimp"],
+	"spacecraft": ["spacecreft"],
+	"stratum": ["strata"],
+	"swine": ["swine"],
+	"thesis": ["theses"],
+	"thief": ["thieves"],
+	"tooth": ["teeth"],
+	"trout": ["trout"],
+	"vortex": ["vortices"],
+	"watercraft": ["watercreft"],
+	"wife": ["wives"],
+	"wolf": ["wolves"],
+	"woman": ["women"],
+
+    };
