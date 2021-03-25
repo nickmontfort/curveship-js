4b6720761780da5372477aa60abf5dfb77e127d5
joanneyu <joanneyu@mit.edu>
2021-03-25 02:43:10 -0400

    implemented parts of

--- examples/afternoon-snack.js
+++ examples/afternoon-snack.js
@@ -2,7 +2,7 @@
 
 var metadata = { title: "Afternoon Snack", author: "Joanne Yuan", date: "2021",
 instructions: "Click below or add your own parameters to the URL to change the “spin” and to create variation in the narrative discourse. You have to use the official names of “actors” for narrator and narratee. Examples:",
-examples: [ "i=x",
+examples: [ "i=x", "i=woman", "i=man", "i=child",
 "order=retrograde", "speaking=after" ] };
 
 
@@ -11,18 +11,16 @@ place.cave = new Place("a", "cave");
 
 // ACTORS next
 actor.X = new Actor("", "X", spatial.in, place.cave, pronoun.masculine);
-
-// THINGS next
-thing.woman = new Thing("a", "woman facedown in the dirt",  spatial.of, actor.lady);
-thing.man = new Thing("a", "man stabbed through the stomach")
-thing.child = new Thing("a", "child crying in front of his parents")
+actor.woman = new Actor("a", "woman facedown in the dirt",  spatial.in, place.cave, pronoun.feminine);
+actor.man = new Actor("a", "man stabbed through the stomach", spatial.in, place.cave, pronoun.masculine)
+actor.child = new Actor("a", "child", spatial.in, place.cave, pronoun.masculine)
 
 //2
 var WALK = new Event(actor.X, "walk in", place.cave);
-var SEE1 = new Event(actor.X, "see", thing.woman);
-var SEE2 = new Event(actor.X, "see", thing.man);
-var SEE3 = new Event(actor.X, "see", thing.child);
-var SURVEY = new Event(actor.X, "survey", [thing.woman, thing.man, thing.child]);
+var SEE1 = new Event(actor.X, "see", actor.woman);
+var SEE2 = new Event(actor.X, "see", actor.man);
+var SEE3 = new Event(actor.X, "see", actor.child);
+var SURVEY = new Event(actor.X, "survey", [actor.woman, actor.man, actor.child]);
 
 
 var world = new World(place, actor, thing, eventSeq);
