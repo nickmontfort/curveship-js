81210fbe355e9cb564fcc096a031644566dd775c
Alan Zhu <alanyzhu@mit.edu>
2021-05-20 00:47:53 -0400

    add a basic example

---
+++ examples/fruits/narrator.js
@@ -0,0 +1,27 @@
+var spin = {};
+
+let told_by = "a narrator";
+
+names = {};
+
+names.room = new Names("a room", "the room");
+
+names.eater = new Names("a fruit-eater", "the fruit-eater");
+
+names.apple = new NameByClass();
+names.strawberry = new Names("a strawberry", "the strawberry");
+names.cherry = new Names("a cherry", "the cherry");
+
+names.fruit = new CategoryNames("fruit");
+names.berries = new CategoryNames("berry");
+names.humans = new CategoryNames("human");
+names.galaApple = new CategoryNames("apple", "Gala");
+
+vp = {};
+
+vp.eatBerries = new VerbPh("eat");
+vp.eat = new VerbPh("eat");
+vp.eatApple = new VerbPh("eat");
+vp.eatSelf = new VerbPh("eat");
+
+function run() { narrate(title, told_by, world, spin, names, vp); }
