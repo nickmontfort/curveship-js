55306fe41d7bf1c0c1245534564b88aa8f96a443
arthalun <71955169+arthalun@users.noreply.github.com>
2021-08-27 12:57:42 -0400

    Modifications

--- tmp/whale/story.js
+++ tmp/whale/story.js
@@ -34,14 +34,14 @@ thing.hands = new Thing(actor.doug);
 // EVENTS
 
 ev.play_violin = new Event(actor.john, thing.violin);
-ev.whisper = new Event(actor.john);
+ev.whisper = new Event(actor.john, "if Bach cannot persuade a mysterious, perhaps divine, messenger to come out of the whale's belly, nothing else can");
 ev.stop_music = new Event(thing.sound, thing.music);
-ev.talk_3 = new Event(actor.doug);
+ev.talk_3 = new Event(actor.doug, "it would've destroyed the whole building if it had moved more! I have been tolerating this filth and flesh in my house for a month. Enough is enough!");
 ev.stab = new Event(actor.doug, thing.whale);
 ev.enter = new Event(actor.doug, thing.whale);
 ev.out = new Event(actor.doug);
 ev.card = new Event(actor.doug, thing.card, temporal.in, thing.hands);
 ev.stare_3 = new Event([actor.doug, actor.john], thing.card);
-ev.card_content = new Event(thing.card);
+ev.card_content = new Event(thing.card, "if you had any problems or did not receive the messenger, call the number below: +1 (857) 204 0988");
 
 var world = new World(place, actor, category, thing, ev);
