40bd73c9280d334085b6de0b8be301f7d2918e4b
Nick Montfort <nickm@nickm.com>
2021-10-27 21:34:27 -0400

    new "psycho" referring expression example

---
+++ referring/patrick.js
@@ -0,0 +1,36 @@
+var toldBy = "Patrick Bateman";
+
+names.gate = new Names("gate 23");
+names.firstClass = new Names("first class");
+
+names.gateOfficial = new Names("this woman working for the airline");
+names.flightAttendant = new Names("a male flight attendant");
+names.passenger = new Names("a woman");
+
+names.boardingPass = new Names("a boarding pass");
+names.scanner = new BrandNames("a", "barcode scanner", null, "Zebra MS471", null, "fixed-mount");
+names.seat1A = new Names("seat 1A");
+names.seat1B = new Names("seat 1B");
+
+names.jacket = new BrandNames("a", "sportscoat", null, null, "Michael Andrews", "bespoke");
+names.sneakers = new BrandNames("", "sneakers", null, "Nike Air Jordan 4 Retro Kaws", "Flight Club");
+names.pen = new BrandNames("a", "pen", null, "BIC", "K-Mart on Astor Place");
+names.notepad = new BrandNames("a", "memo pad", null, "Mead", "some bodega");
+
+vp.scan = new VerbPh("hold");
+vp.beep = new VerbPh("beep");
+vp.gasp = new VerbPh("gasp");
+vp.pocket = new VerbPh("place");
+vp.depart = new VerbPh("depart");
+vp.board = new VerbPh("walk");
+vp.sit = new VerbPh("sit");
+vp.beSurprised = new VerbPh("react");
+vp.see = new VerbPh("notice");
+vp.confront = new VerbPh("sneer");
+vp.withdraw = new VerbPh("get");
+vp.show = new VerbPh("show");
+vp.apologize = new VerbPh("mutter");
+vp.obtain = new VerbPh("grab");
+vp.request = new VerbPh("say");
+
+function run() { narrate(title, toldBy, world, spin, names, vp); }
